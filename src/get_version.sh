2f7acc9a3fe9f300dc07f65ad2c3b1219a3b7637
qiaofeng1227 <76487013@qq.com>
2022-07-26 09:53:10 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-udo echo "dolibarr version:" $(docker images |grep tuxgasy/dolibarr |awk '{print $2}')  1>> /data/logs/install_version.txt
+sudo echo "dolibarr version:" $(docker images |grep tuxgasy/dolibarr |awk '{print $2}')  1>> /data/logs/install_version.txt
